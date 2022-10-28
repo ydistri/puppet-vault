1c6c616aa26d9ae5aa2bb2b473dd380e2d00f96b
Tim Meusel <tim@bastelfreak.de>
2022-10-28 09:16:46 +0200

    puppet-lint: autofix

--- manifests/init.pp
+++ manifests/init.pp
@@ -75,61 +75,59 @@
 #   The version of Vault to install
 #
 class vault (
-  $user                                = $::vault::params::user,
-  $manage_user                         = $::vault::params::manage_user,
-  $group                               = $::vault::params::group,
-  $manage_group                        = $::vault::params::manage_group,
-  $bin_dir                             = $::vault::params::bin_dir,
-  $config_dir                          = $::vault::params::config_dir,
-  $manage_config_file                  = $::vault::params::manage_config_file,
-  $config_mode                         = $::vault::params::config_mode,
+  $user                                = $vault::params::user,
+  $manage_user                         = $vault::params::manage_user,
+  $group                               = $vault::params::group,
+  $manage_group                        = $vault::params::manage_group,
+  $bin_dir                             = $vault::params::bin_dir,
+  $config_dir                          = $vault::params::config_dir,
+  $manage_config_file                  = $vault::params::manage_config_file,
+  $config_mode                         = $vault::params::config_mode,
   $purge_config_dir                    = true,
-  $download_url                        = $::vault::params::download_url,
-  $download_url_base                   = $::vault::params::download_url_base,
-  $download_extension                  = $::vault::params::download_extension,
-  $service_name                        = $::vault::params::service_name,
-  $service_enable                      = $::vault::params::service_enable,
-  $service_ensure                      = $::vault::params::service_ensure,
-  $service_provider                    = $::vault::params::service_provider,
-  Boolean $manage_repo                 = $::vault::params::manage_repo,
-  $manage_service                      = $::vault::params::manage_service,
-  $manage_service_file                 = $::vault::params::manage_service_file,
-  Hash $storage                        = $::vault::params::storage,
-  $manage_storage_dir                  = $::vault::params::manage_storage_dir,
-  Variant[Hash, Array[Hash]] $listener = $::vault::params::listener,
-  Optional[Hash] $ha_storage           = $::vault::params::ha_storage,
-  Optional[Hash] $seal                 = $::vault::params::seal,
-  Optional[Boolean] $disable_cache     = $::vault::params::disable_cache,
-  Optional[Hash] $telemetry            = $::vault::params::telemetry,
-  Optional[String] $default_lease_ttl  = $::vault::params::default_lease_ttl,
-  Optional[String] $max_lease_ttl      = $::vault::params::max_lease_ttl,
-  $disable_mlock                       = $::vault::params::disable_mlock,
-  $manage_file_capabilities            = $::vault::params::manage_file_capabilities,
+  $download_url                        = $vault::params::download_url,
+  $download_url_base                   = $vault::params::download_url_base,
+  $download_extension                  = $vault::params::download_extension,
+  $service_name                        = $vault::params::service_name,
+  $service_enable                      = $vault::params::service_enable,
+  $service_ensure                      = $vault::params::service_ensure,
+  $service_provider                    = $vault::params::service_provider,
+  Boolean $manage_repo                 = $vault::params::manage_repo,
+  $manage_service                      = $vault::params::manage_service,
+  $manage_service_file                 = $vault::params::manage_service_file,
+  Hash $storage                        = $vault::params::storage,
+  $manage_storage_dir                  = $vault::params::manage_storage_dir,
+  Variant[Hash, Array[Hash]] $listener = $vault::params::listener,
+  Optional[Hash] $ha_storage           = $vault::params::ha_storage,
+  Optional[Hash] $seal                 = $vault::params::seal,
+  Optional[Boolean] $disable_cache     = $vault::params::disable_cache,
+  Optional[Hash] $telemetry            = $vault::params::telemetry,
+  Optional[String] $default_lease_ttl  = $vault::params::default_lease_ttl,
+  Optional[String] $max_lease_ttl      = $vault::params::max_lease_ttl,
+  $disable_mlock                       = $vault::params::disable_mlock,
+  $manage_file_capabilities            = $vault::params::manage_file_capabilities,
   $service_options                     = '',
-  $num_procs                           = $::vault::params::num_procs,
-  $install_method                      = $::vault::params::install_method,
-  $package_name                        = $::vault::params::package_name,
-  $package_ensure                      = $::vault::params::package_ensure,
-  $download_dir                        = $::vault::params::download_dir,
-  $manage_download_dir                 = $::vault::params::manage_download_dir,
-  $download_filename                   = $::vault::params::download_filename,
-  $version                             = $::vault::params::version,
-  $os                                  = $::vault::params::os,
-  $arch                                = $::vault::params::arch,
-  Optional[Boolean] $enable_ui         = $::vault::params::enable_ui,
+  $num_procs                           = $vault::params::num_procs,
+  $install_method                      = $vault::params::install_method,
+  $package_name                        = $vault::params::package_name,
+  $package_ensure                      = $vault::params::package_ensure,
+  $download_dir                        = $vault::params::download_dir,
+  $manage_download_dir                 = $vault::params::manage_download_dir,
+  $download_filename                   = $vault::params::download_filename,
+  $version                             = $vault::params::version,
+  $os                                  = $vault::params::os,
+  $arch                                = $vault::params::arch,
+  Optional[Boolean] $enable_ui         = $vault::params::enable_ui,
   Optional[String] $api_addr           = undef,
   Hash $extra_config                   = {},
-) inherits ::vault::params {
-
+) inherits vault::params {
   # lint:ignore:140chars
   $real_download_url = pick($download_url, "${download_url_base}${version}/${package_name}_${version}_${os}_${arch}.${download_extension}")
   # lint:endignore
 
-  contain ::vault::install
-  contain ::vault::config
-  contain ::vault::service
+  contain vault::install
+  contain vault::config
+  contain vault::service
 
   Class['vault::install'] -> Class['vault::config']
   Class['vault::config'] ~> Class['vault::service']
-
 }
