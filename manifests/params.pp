6f6dc23c7386bebfc73078fa3207ccfa638c75f9
Rudy Gevaert <66934+rgevaert@users.noreply.github.com>
2022-12-08 23:06:45 +0100

    Move static data to init

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,59 +4,6 @@
 # @api private
 #
 class vault::params {
-  $user               = 'vault'
-  $manage_user        = true
-  $group              = 'vault'
-  $manage_group       = true
-  $config_dir         = '/etc/vault'
-  $config_mode        = '0750'
-  $manage_config_file = true
-  $download_url       = undef
-  $download_url_base  = 'https://releases.hashicorp.com/vault/'
-  $download_extension = 'zip'
-  $version            = '1.4.2'
-  $service_name       = 'vault'
-  $num_procs          = $facts['os']['processorcount']
-  $package_name       = 'vault'
-  $package_ensure     = 'installed'
-
-  $download_dir        = '/tmp'
-  $manage_download_dir = false
-  $download_filename   = 'vault.zip'
-
-  # storage and listener are mandatory, we provide some sensible
-  # defaults here
-  $storage             = { 'file' => { 'path' => '/var/lib/vault' } }
-  $manage_storage_dir  = false
-  $listener            = {
-    'tcp' => {
-      'address'     => '127.0.0.1:8200',
-      'tls_disable' => 1,
-    },
-  }
-
-  $enable_ui          = undef
-
-  # These should always be undef as they are optional settings that
-  # should not be configured unless explicitly declared.
-  $ha_storage           = undef
-  $seal                 = undef
-  $disable_cache        = undef
-  $telemetry            = undef
-  $default_lease_ttl    = undef
-  $max_lease_ttl        = undef
-  $disable_mlock        = undef
-  $service_registration = undef
-
-  $manage_file_capabilities = undef
-
-  $manage_service = true
-
-  $service_enable = true
-  $service_ensure = 'running'
-
-  $service_provider = $facts['service_provider']
-
   case $facts['os']['architecture'] {
     'aarch64':        { $arch = 'arm64' }
     /(x86_64|amd64)/: { $arch = 'amd64' }
